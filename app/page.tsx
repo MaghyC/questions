99a06b7a2203bfbf9e801cec21335a6ba069cefd
scls-cs <tong.hu@scls-sh.org>
2024-11-26 09:15:35 +0800

    add data demo table

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,54 @@
 // src/app/page.tsx
-import Link from 'next/link';
-import { questions, Question } from './questions/data';
+import Link from "next/link";
+import { questions, Question } from "./questions/data";
+import DataTableDemo from "./questions/table";
 //import Cookies from 'js-cookie';
 
 export default function Home() {
   const statusColors = {
-    correct: 'bg-green-500',
-    wrong: 'bg-red-500',
-    pending: 'bg-gray-400'
+    correct: "bg-green-500",
+    wrong: "bg-red-500",
+    pending: "bg-gray-400",
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <table className="min-w-full border-collapse">
-        <thead>
-          <tr>
-            <th className="p-2 border">Status</th>
-            <th className="p-2 border">Question</th>
-            <th className="p-2 border">Unit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {questions.map((question) => (
-            <tr key={question.id} >
-              <td className="p-2 border">
-                <div 
-                  className={`w-4 h-4 rounded-full ${statusColors[question.status]}`}
-                  title={question.status}
-                />
-              </td>
-              <td className="p-2 border">
-                <Link 
-                  href={`/questions?name=${encodeURIComponent(question.name.toLowerCase())}`}
-                  className="text-blue-600 hover:text-blue-800 hover:underline"
-                >
-                  {question.name}
-                </Link>
-              </td>
-              <td className="p-2 border">
-                {question.unit}
-              </td>
+    <div>
+      <DataTableDemo />
+      <div className="container mx-auto p-4">
+        <table className="min-w-full border-collapse">
+          <thead>
+            <tr>
+              <th className="p-2 border">Status</th>
+              <th className="p-2 border">Question</th>
+              <th className="p-2 border">Unit</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {questions.map((question) => (
+              <tr key={question.id}>
+                <td className="p-2 border">
+                  <div
+                    className={`w-4 h-4 rounded-full ${
+                      statusColors[question.status]
+                    }`}
+                    title={question.status}
+                  />
+                </td>
+                <td className="p-2 border">
+                  <Link
+                    href={`/questions?name=${encodeURIComponent(
+                      question.name.toLowerCase()
+                    )}`}
+                    className="text-blue-600 hover:text-blue-800 hover:underline">
+                    {question.name}
+                  </Link>
+                </td>
+                <td className="p-2 border">{question.unit}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
